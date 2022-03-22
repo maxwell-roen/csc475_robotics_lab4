88af85900e54d6ceba56c6ae00dcce392f10304f
Max Roen <maxwell-roen@users.noreply.github.com>
2022-03-21 21:32:38 -0400

    Update detect_ball.py

--- scripts/detect_ball.py
+++ scripts/detect_ball.py
@@ -18,14 +18,27 @@ def get_image(ros_img):
 	# convert the ros img to an opencv img
 	rgb_img = CvBridge().imgmsg_to_cv2(ros_img, 'rgb8')
 	img_received = True
-	
+	# cv2.imwrite('capture.jpeg', rgb_img)
 
-# just do this for now and make sure structure is working.
-# will take in the rgb_img
-# it is, let's do the img processing...
+# do the actual img processing
 def detect_ball(rgb_img):
-	#return np.zeros((720, 1280, 1), dtype='uint8')
-	return rgb_img
+	# first convert to HSV
+	hsv = cv2.cvtColor(rgb_img, cv2.COLOR_RGB2HSV)
+	# define color ranges for the ball. not completely happy with these: having a hard time picking up
+	# the black (logo) and white (lines) parts of the ball w/o introducing noise.
+	lower_yellow_hsv = np.array([28, 8, 98])
+	upper_yellow_hsv = np.array([65, 255, 255])
+	# filter the img
+	yellow_mask = cv2.inRange(hsv, lower_yellow_hsv, upper_yellow_hsv)
+	
+	# use a bitwise and mask to declare an area of interest, filtering out noise along the edges.
+	mask = np.zeros((rgb_img.shape[0], rgb_img.shape[1], 1), dtype='uint8')
+	area_of_interest = cv2.rectangle(mask, (200, 150), (1152, 648), 255, -1)
+
+	# bitwise and the mask with the img
+	filtered_img = cv2.bitwise_and(yellow_mask, area_of_interest, mask=area_of_interest)
+	
+	return filtered_img
 
 
 if __name__ == '__main__':
@@ -50,4 +63,3 @@ if __name__ == '__main__':
 			img_pub.publish(img_msg)
 		# pause til next loop
 		rate.sleep()
-			
